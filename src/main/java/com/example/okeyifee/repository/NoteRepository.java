3e97c21e76f701f5cd14c9fb8bf37a9c6e0265ec
okeyifee <okezieokechukwu94@gmail.com>
2020-11-05 10:04:25 +0100

    create note repository

--- src/main/java/com/example/okeyifee/repository/NoteRepository.java
+++ src/main/java/com/example/okeyifee/repository/NoteRepository.java
@@ -2,20 +2,24 @@ package com.example.okeyifee.repository;
 
 
 import com.example.okeyifee.models.Note;
+import com.example.okeyifee.dto.NoteDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RequestMapping
 public interface NoteRepository extends JpaRepository<Note, Long>{
 
-    Note findAllByTitle(String title);
-    Note findAllByCreatedAt(Date date);
-    Note findAllByContentContaining(String matcher);
-    Note findAllByTitleContains(String matcher);
-    Note findNoteByCreatedAt(Date date);
-    Note findANoteByTitleContains(String matcher);
-    Note findNoteByTitle(String title);
+    List<Note> findAllByTitle(String title);
+    List<Note>  findAllByCreatedAt(LocalDateTime date);
+    List<Note>  findAllByContentContaining(String matcher);
+    List<Note>  findAllByTitleContains(String matcher);
+    Note findNoteByCreatedAt(LocalDateTime date);
+    Note findNoteByTitleContains(String matcher);
+    List<Note> findNoteByTitle(String title);
+    Note findNoteById(Long id);
 
 }
