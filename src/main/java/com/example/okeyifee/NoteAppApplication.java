2d8c71769decf2f631a7ea7d5653a3595ed54f44
okeyifee <okezieokechukwu94@gmail.com>
2020-11-05 10:07:41 +0100

    enabled configuration and add password encoder bean

--- src/main/java/com/example/okeyifee/NoteAppApplication.java
+++ src/main/java/com/example/okeyifee/NoteAppApplication.java
@@ -2,12 +2,21 @@ package com.example.okeyifee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@Configuration
 public class NoteAppApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(NoteAppApplication.class, args);
     }
 
+    @Bean
+    PasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
