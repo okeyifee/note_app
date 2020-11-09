c7cee68cdbc8264a937b62ecaa436fe40d958178
okeyifee <okezieokechukwu94@gmail.com>
2020-11-09 03:19:37 +0100

    create Endpoints

--- src/main/java/com/example/okeyifee/controller/NoteController.java
+++ src/main/java/com/example/okeyifee/controller/NoteController.java
@@ -1,13 +1,13 @@
 package com.example.okeyifee.controller;
 
-import com.example.okeyifee.dto.NoteDto;
-import com.example.okeyifee.models.Note;
+import com.example.okeyifee.dto.NoteDTO;
+import com.example.okeyifee.payload.ApiResponse;
 import com.example.okeyifee.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/notes")
@@ -15,25 +15,63 @@ public class NoteController{
 
     private NoteService noteService;
 
-
     @Autowired
     public NoteController(NoteService noteService) {
         this.noteService = noteService;
     }
 
     @PostMapping("/create")
-    public void createNote(@RequestBody Note note) {
-        noteService.save(note);
-        System.out.println();
+    public ResponseEntity<ApiResponse> createNote(@Valid @RequestBody NoteDTO noteDto) {
+        return noteService.save(noteDto);
     }
-//
-//    @PostMapping
-//    ResponseEntity<Person> create(@RequestBody @Validated Person person) {
 
-    @GetMapping("/retrieve")
-    public List<NoteDto> retrieveNote(@RequestBody NoteDto notedto) {
-        List<NoteDto> answer = noteService.findNoteUsingTitle(notedto.getTitle());
-        return answer;
+    @GetMapping("/retrieve/title")
+    public ResponseEntity<ApiResponse> retrieveNoteByTitle(@Valid @RequestBody NoteDTO noteDto) {
+        return noteService.findNoteUsingTitle(noteDto);
+    }
+
+    @GetMapping("/retrieve_All")
+    public ResponseEntity<ApiResponse> retrieveAllNotes() { /////////////
+        return noteService.findAllNotes();
+    }
+
+    @GetMapping("/retrieve/id")
+    public ResponseEntity<ApiResponse> retrieveNoteById(@Valid @RequestBody NoteDTO noteDto) {
+        return noteService.findNoteById(noteDto);
+    }
+
+    @GetMapping("/edit/{id}")
+    public ResponseEntity<ApiResponse> editNoteById(@Valid @RequestBody NoteDTO noteDto) {
+        return noteService.editNoteById(noteDto);
+    }
+
+    @GetMapping("/delete/{id}")
+    public ResponseEntity<ApiResponse> deleteNoteById(@Valid @RequestBody NoteDTO noteDto) {
+        return noteService.deleteNoteById(noteDto);
+    }
+
+    @GetMapping("/delete/title")
+    public ResponseEntity<ApiResponse> deleteNoteByTitle(@Valid @RequestBody NoteDTO noteDto) {
+        return noteService.deleteNoteByTitle(noteDto);
+    }
+
+    @GetMapping("/retrieve/creation-date")
+    public ResponseEntity<ApiResponse> findNoteByCreatedAt(@Valid @RequestBody NoteDTO noteDto) {
+        return noteService.findAllByCreatedAt(noteDto);
+    }
+
+    @GetMapping("/retrieve/content")
+    public ResponseEntity<ApiResponse> findNoteByContentContaining(@Valid @RequestBody NoteDTO noteDto) {
+        return noteService.findAllByContentContaining(noteDto);
+    }
+
+    @GetMapping("retrieve/title-contains")
+    public ResponseEntity<ApiResponse> findNoteByTitleContains(@Valid @RequestBody NoteDTO noteDto) {
+        return noteService.findAllByTitleContains(noteDto);
     }
 }
+
+
+
+
 
