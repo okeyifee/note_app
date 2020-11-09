7f94d1dd185f1dfa470173ca49ffe276fef7a3ca
okeyifee <okezieokechukwu94@gmail.com>
2020-11-09 03:19:00 +0100

    modified Main class

--- src/main/java/com/example/okeyifee/NoteAppApplication.java
+++ src/main/java/com/example/okeyifee/NoteAppApplication.java
@@ -6,7 +6,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableSwagger2
 public class NoteAppApplication{
 
