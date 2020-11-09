3c17c5c6676095ef75c2cf3d2f02c153b092346e
okeyifee <okezieokechukwu94@gmail.com>
2020-11-09 03:20:53 +0100

    modified Note repository

--- src/main/java/com/example/okeyifee/repository/NoteRepository.java
+++ src/main/java/com/example/okeyifee/repository/NoteRepository.java
@@ -1,25 +1,37 @@
 package com.example.okeyifee.repository;
 
-
 import com.example.okeyifee.models.Note;
-import com.example.okeyifee.dto.NoteDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.sql.Date;
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping
 public interface NoteRepository extends JpaRepository<Note, Long>{
 
-    List<Note> findAllByTitle(String title);
-    List<Note>  findAllByCreatedAt(LocalDateTime date);
+    Note save(Note note);
+
+    @Query(value="SELECT * FROM notes u WHERE deleted = false AND created_at = ?1", nativeQuery = true)
+    List<Note>  findAllByCreatedAt(Timestamp date);
+
+    @Query(value="SELECT * FROM notes u WHERE deleted = false AND content = ?1", nativeQuery = true)
     List<Note>  findAllByContentContaining(String matcher);
+
+    @Query(value="SELECT * FROM notes u WHERE deleted = false AND title = ?1", nativeQuery = true)
     List<Note>  findAllByTitleContains(String matcher);
-    Note findNoteByCreatedAt(LocalDateTime date);
+
+    @Query(value="SELECT * FROM notes u WHERE deleted = false AND title = ?1", nativeQuery = true)
     Note findNoteByTitleContains(String matcher);
-    List<Note> findNoteByTitle(String title);
+
+    @Query(value="SELECT * FROM notes u WHERE deleted = false AND id = ?1", nativeQuery = true)
     Note findNoteById(Long id);
 
+    @Query(value="SELECT * FROM notes u WHERE deleted = false AND title = ?1", nativeQuery = true)
+    List<Note> findNoteByTitle(String Title);
+
+    @Query(value="SELECT * FROM notes u WHERE deleted = false", nativeQuery = true)
+    List<Note> findAllNotes();
 }
