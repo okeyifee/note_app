5defad09e3dd21212182b33e13bbc8256e4a8c6d
okeyifee <okezieokechukwu94@gmail.com>
2020-11-11 04:56:09 +0100

    remove unused import

--- src/main/java/com/example/okeyifee/controller/EmailController.java
+++ src/main/java/com/example/okeyifee/controller/EmailController.java
@@ -7,14 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
-//@RequestMapping("/auth")
-public class EmailController{
+public class EmailController {
 
     UserEmailService userEmailService;
 
@@ -22,6 +20,7 @@ public class EmailController{
     public EmailController(UserEmailService userEmailService) {
         this.userEmailService = userEmailService;
     }
+
     /**
      * Method to send Account Activation link
      * @return ResponseEntity<>(response, HttpStatus)
