dc8b1a9083cb98800241e283e9f8751fe578c8f6
okeyifee <okezieokechukwu94@gmail.com>
2020-11-11 08:30:28 +0100

    add H2 database for test

--- src/test/java/com/example/okeyifee/NoteAppApplicationTests.java
+++ src/test/java/com/example/okeyifee/NoteAppApplicationTests.java
@@ -1,8 +1,13 @@
 package com.example.okeyifee;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @SpringBootTest
 class NoteAppApplicationTests{
 
